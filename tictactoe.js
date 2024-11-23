5e1d71c9b588d495f57ddd6824e8d775a7f99810
Sindri <125163217+sindrilear@users.noreply.github.com>
2024-11-23 21:08:31 +0000

    Implement page design and basic functionality for tic tac toe game

--- tictactoe.js
+++ tictactoe.js
@@ -1,118 +1,153 @@
-const gameboard = {
-    gameboardArray:
-[
-    ["''", "''", "''"],
-    ["''", "''", "''"],
-    ["''", "''", "''"]
-]
-}
-
 const player1 = {
     playerSymbol: "x",
-    playerStatus: [0, 0, 0, 0, 0, 0, 0 ,0]
-}
+    playerName: "x",
+    playerStatus: [0, 0, 0, 0, 0, 0, 0 ,0],
+    playerScore: 0,
+    resetStatus() { 
+        this.playerStatus = [0, 0, 0, 0, 0, 0, 0 ,0];
+    }
+    changeName(name) {
+        this.playerName = name;
+    }
+};
 
 const player2 = {
     playerSymbol: "o",
-    playerStatus: [0, 0, 0, 0, 0, 0, 0 ,0]
+    playerName: "o",
+    playerStatus: [0, 0, 0, 0, 0, 0, 0 ,0],
+    playerScore: 0,
+    resetStatus() { 
+        this.playerStatus = [0, 0, 0, 0, 0, 0, 0 ,0];
+    }
+    changeName(name) {
+        this.playerName = name;
+    }
 }
 
-function logMoves() {
-console.log(
-"Press 1 for top-left \n\
-Press 2 for top-center\nPress 3 for top-right\n\
-Press 4 for center-left\nPress 5 for center\n\
-Press 6 for center-right\nPress 7 for bottom-left\n\
-Press 8 for bottom-center\nPress 9 for bottom-right");
-}
-
-function  playGame () {
-let i = 0;
 let player1turn = true;
 let choices = [];
+let turns = 0;
+let tie = 0;
 
-while (i < 9){
-logMoves();
-if (player1turn === true) {
-    player = player1.playerSymbol;
-    playerStatus = player1.playerStatus;
-    player1turn = false;
+function addScore (playerScore, playerSymbol) {
+const scoreX = document.getElementById("scoreX")
+const scoreY = document.getElementById("scoreY")
+if  (playerSymbol === "x") {
+    scoreX.innerHTML = playerScore
 } else {
-    player = player2.playerSymbol;
-    playerStatus = player2.playerStatus;
+    scoreY.innerHTML = playerScore
+}};
+
+function resetGame() {
+    choices = [];
+    turns = 0;
     player1turn = true;
+    player1.resetStatus();
+    player2.resetStatus();
+    for (i = 1; i < 10; i++) {
+        grid = document.getElementById(i)
+        grid.innerHTML = "";
+    }
 }
-isTrue = true;
-playerChoice = "";
-    while(isTrue) {
-        let playerChoice = parseInt(prompt(`Make your move....${player}`))
-        if (choices.includes(playerChoice)) {
-            console.log("This move has already been made")
-        } else if (playerChoice > 9 || playerChoice <= 0) {
-            console.log("Input is incorrect")
-        } else {
+
+function announcePlayer(playertoannounce) {
+    const player = document.getElementById("announce")
+    if (playertoannounce === "x") {
+        player.innerHTML = `It's player ${player1.playerName} turn!`;
+    } else {
+        player.innerHTML = `It's player ${player2.playerName} turn!`;
+    }
+}
+
+announcePlayer("x");
+
+const gamegrid = document.querySelector(".gamegrid")
+
+gamegrid.addEventListener('click', (ev) => {
+    if (player1turn === true) {
+        player = player1.playerSymbol;
+        playerStatus = player1.playerStatus;
+        playerScore = player1.playerScore;
+        player1turn = false;
+    } else {
+        player = player2.playerSymbol;
+        playerStatus = player2.playerStatus;
+        playerScore = player2.playerScore;
+        player1turn = true;
+    }
+    playerChoice = parseInt(ev.target.id);
+    const grid = document.getElementById(`${playerChoice}`)
+    const announce = document.getElementById("announce")
+    const scoreTie = document.getElementById("scoreTie")
+
+    if (choices.includes(playerChoice)) {
+       announce.innerHTML = "This move has already been made"
+    } else {
         choices.push(playerChoice)
+        grid.innerHTML = player;
+        player1turn ? announcePlayer(player1.playerSymbol) :  announcePlayer(player2.playerSymbol);
+        console.log(playerStatus);
         switch (playerChoice) {
             case 1:
-                gameboard.gameboardArray[0][0] = player;
                 playerStatus[0]++;
                 playerStatus[3]++;
                 playerStatus[6]++;
                 break;
             case 2:
-                gameboard.gameboardArray[0][1] = player;
                 playerStatus[0]++;
                 playerStatus[4]++;
                 break;
             case 3:
-                gameboard.gameboardArray[0][2] = player;
                 playerStatus[0]++;
                 playerStatus[5]++;
                 playerStatus[7]++;
                 break;
             case 4:
-                gameboard.gameboardArray[1][0] = player;
                 playerStatus[1]++;
                 playerStatus[3]++;
                 break;
             case 5:
-                gameboard.gameboardArray[1][1] = player;
                 playerStatus[1]++;
                 playerStatus[4]++;
                 playerStatus[6]++;
                 playerStatus[7]++;
                 break;
             case 6:
-                gameboard.gameboardArray[1][2] = player;
                 playerStatus[1]++;
                 playerStatus[5]++;
                 break;
             case 7:
-                gameboard.gameboardArray[2][0] = player;
                 playerStatus[2]++;
                 playerStatus[3]++;
                 playerStatus[7]++;
                 break;
             case 8:
-                gameboard.gameboardArray[2][1] = player;
                 playerStatus[2]++;
                 playerStatus[4]++;
                 break;
             case 9:
-                gameboard.gameboardArray[2][2] = player;
                 playerStatus[2]++;
                 playerStatus[5]++;
                 playerStatus[6]++;
                 break;
         }
-        isTrue = false;
-    }}
-
-if (playerStatus.includes(3)) {
-    console.log(`Player ${player} wins!`);
-    console.table(gameboard.gameboardArray);
-    break;
-}
-console.table(gameboard.gameboardArray);
-i++;
-}}
+        turns++;
+        if (turns === 9) {
+            announce.innerHTML = "It's a tie!"
+            tie++;
+            scoreTie.innerHTML = tie;
+            resetGame();
+        } else if (playerStatus.includes(3)) {
+            announce.innerHTML = `Player ${player} wins!`
+            playerScore++;
+            if (player ===  "x") {
+                player1.playerScore++;
+                addScore(playerScore, player);
+            } else {
+                player2.playerScore++;
+                addScore(playerScore, player);
+            }
+            resetGame();
+        }
+    }
+});
